66a5218e766262311b2fc9821a128401f3366dc9
Ron Ward <defroged@gmail.com>
2024-07-06 13:14:05 +0900

    change host fix3

--- api/send_email.js
+++ api/send_email.js
@@ -114,7 +114,7 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-});;
+});
 
   const mailOptions = {
     from: 'hello@bluestar-english.com',
